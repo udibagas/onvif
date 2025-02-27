6043ffc7521cd4720fbba91d50d4af3959d5bb20
Bagas Udi Sahsangka <udibagas@gmail.com>
2025-02-27 14:54:21 +0700

    update

--- test.js
+++ test.js
@@ -16,14 +16,41 @@ device
 
     let profile = device.getCurrentProfile();
     console.dir(profile, { depth: null });
-    return device.fetchSnapshot();
+
+    const outputFilePath = "snapshot.jpg";
+
+    // Create an FFmpeg command
+    ffmpeg(profile.stream.rtsp)
+      .on("start", (commandLine) => {
+        console.log("FFmpeg process started with command: " + commandLine);
+      })
+      .on("error", (err) => {
+        console.error("Error occurred: " + err.message);
+      })
+      .on("end", () => {
+        console.log("Snapshot captured successfully!");
+      })
+      .screenshots({
+        timestamps: [0], // Capture the first frame (0 seconds)
+        filename: outputFilePath,
+        folder: "./", // Save in the current directory
+        size: "640x480", // Optional: Set the resolution of the snapshot
+      });
   })
-  .then((res) => {
-    console.log(res);
-    // Save the data to a file
-    fs.writeFileSync("snapshot.jpg", res.body, { encoding: "binary" });
-    console.log("Done!");
-  })
+  // .then((res) => {
+  //   console.log(res);
+  //   // Save the data to a file
+  //   fs.writeFileSync("snapshot.jpg", res.body, { encoding: "binary" });
+  //   console.log("Done!");
+  // })
   .catch((error) => {
     console.error(error);
   });
+
+const ffmpeg = require("fluent-ffmpeg");
+const fs = require("fs");
+
+// RTSP stream URL
+const rtspUrl = "rtsp://username:password@your_cctv_ip:port/stream";
+
+// Output file path for the snapshot
